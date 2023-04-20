b85c643448db525221e6fae0bafd9021b676e59a
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-04-20 15:25:24 +0300

    в процессе

--- test/lab3.py
+++ test/lab3.py
@@ -5,7 +5,7 @@ import re
 CLASSES_OF_TOKENS = ['W', 'I', 'O', 'R', 'N', 'C']
 
 def is_identifier(token):
-    return ((token in inverse_tokens) and re.match(r'^I\d+$', inverse_tokens[token])) or re.match(r'^M\d+$', token)
+    return ((token in inverse_tokens) and re.match(r'^I\d+$', inverse_tokens[token])) or re.match(r'^M\d+$', token) or token in ['String[]', 'args']
 
 def is_constant(token):
     return ((token in inverse_tokens) and re.match(r'^C\d+$', inverse_tokens[token])) or ((token in inverse_tokens) and re.match(r'^N\d+$', inverse_tokens[token])) or token.isdigit()
@@ -33,8 +33,9 @@ replace = {'in.nextInt()': 'scanf', 'in.nextDouble()': 'scanf', 'in.nextLine()':
 # файл, содержащий обратную польскую запись
 f = open('reverse_polish_entry.txt', 'r')
 inp_seq = f.read()
-inp_seq = inp_seq.replace("public static void 2 АЭМ String args ","")
-inp_seq = inp_seq.replace("System.out.println ","printf")
+inp_seq = inp_seq.replace("public static void 2 АЭМ String args ","String[] args НП ")
+inp_seq = inp_seq.replace("main"," КП")
+inp_seq = inp_seq.replace("System.out.println "," printf ")
 f.close()
 
 t = re.findall(r'(?:\'[^\']*\')|(?:"[^"]*")|(?:[^ ]+)', inp_seq)
@@ -53,18 +54,18 @@ while i < len(t):
     elif t[i] == 'НП':
         stack.pop()
         stack.pop()
-        arg1 = stack.pop()
-        out_seq += f'public static function {arg1}'
+        #arg1 = stack.pop()
+        out_seq += 'int main ()'
         is_func = True
     elif t[i] == 'КП':
-        out_seq += '}'
+        out_seq += '}\n'
     elif t[i] == 'КО':
         stack.pop()
         stack.pop()
     elif t[i] == 'УПЛ':
         arg1 = stack.pop()
         arg2 = stack.pop()
-        out_seq += f'if {arg2} {arg1}\n'
+        out_seq += f'if (!({arg2})) goto {arg1};\n'
     elif t[i] == 'БП':
         arg1 = stack.pop()
         out_seq += f'goto {arg1}\n'
@@ -104,29 +105,31 @@ while i < len(t):
         out_seq += t[i] + stack.pop() + '\n'
     i += 1
 
-out_seq = re.sub(r'(M\d+): if \(!\(\((.*)\)\)\): goto (M\d+)(?:\n|\n((?:.|\n)+)\n)goto \1\n\3: ', r'while \2 {\n\4\n}\n', out_seq)
+#out_seq = re.sub(r'(M\d+): if \(!\(\((.*)\)\)\): goto (M\d+)(?:\n|\n((?:.|\n)+)\n)goto \1\n\3: ', r'while \2 {\n\4\n}\n', out_seq)
+out_seq = re.sub(r'if \(\!\(\((.*?)\)\)\) goto (M\d+);\n\2: printf\((.*?)\);', r'if (\1) {\n  printf(\3);\n}', out_seq)
 out_seq = re.sub(r'if \(!\(\((.*)\)\)\): goto (M\d+)(?:\n|\n((?:.|\n)+)\n)goto (M\d+)\n\2: ((?:.|\n)+)\n?\4: ', r'if \1 {\n\3\n} else {\n\5\n}\n', out_seq)
 out_seq = re.sub(r'if \(!\(\((.*)\)\)\): goto (M\d+)(?:\n|\n((?:.|\n)+)\n)\2: ', r'if \1 {\n\3\n}\n', out_seq)
 
-c = 0
-a = out_seq.split('\n')
-for i in range(len(a)):
-    if len(a[i]) == 0:
-        continue
-    if a[i][0] == '}':
-        c -= 1
-    a[i] = 4*c*' ' + a[i]
-    if a[i][len(a[i]) - 1] == '{':
-        a[i] = re.sub(r'[ ]*{$', r':', a[i])
-        c += 1
-out_seq = '\n'.join(a)
-out_seq = re.sub(r'}[ ]*', r'', out_seq)
-a = out_seq.split('\n')
-a = [i for i in a if len(i.strip()) > 0]
-out_seq = '\n'.join(a)
-out_seq = re.sub(r'([^ ]+) = input\(([^\)]*)\)\n\1 = int\(\1\)', r'\1 = int(input(\2))', out_seq)
-while re.search(r'= \(([^\)]+)\)\n', out_seq):
-    out_seq = re.sub(r'= \(([^\)]+)\)\n', r'= \1\n', out_seq)
+
+# c = 0
+# a = out_seq.split('\n')
+# for i in range(len(a)):
+#     if len(a[i]) == 0:
+#         continue
+#     if a[i][0] == '}':
+#         c -= 1
+#     a[i] = 4*c*' ' + a[i]
+#     if a[i][len(a[i]) - 1] == '{':
+#         a[i] = re.sub(r'[ ]*{$', r':', a[i])
+#         c += 1
+# out_seq = '\n'.join(a)
+# out_seq = re.sub(r'}[ ]*', r'', out_seq)
+# a = out_seq.split('\n')
+# a = [i for i in a if len(i.strip()) > 0]
+# out_seq = '\n'.join(a)
+# out_seq = re.sub(r'([^ ]+) = input\(([^\)]*)\)\n\1 = int\(\1\)', r'\1 = int(input(\2))', out_seq)
+# while re.search(r'= \(([^\)]+)\)\n', out_seq):
+#     out_seq = re.sub(r'= \(([^\)]+)\)\n', r'= \1\n', out_seq)
 
 # файл, содержащий текст на выходном языке программирования
 f = open('C++.txt', 'w')
