8b72665d155e71aea6a66adf6f590d455210d29a
Jakepps <96076243+Jakepps@users.noreply.github.com>
2023-05-10 23:15:18 +0300

    Синтаксический анализатор

--- Translator/Trans.py
+++ Translator/Trans.py
@@ -5,6 +5,7 @@ import tkinter.scrolledtext as st
 from tkinter import ttk
 import tkinter as tk
 import os
+import re
 
 CLASSES_OF_TOKENS = ['W', 'I', 'O', 'R', 'N', 'C']
 
@@ -55,6 +56,11 @@ def prog1():
     # файл, содержащий текст на входном языке программирования
     f = open('java.txt', 'r')
     input_sequence = f.read()
+    input_sequence = re.sub(r"(\w)\+\+", r"\1 = \1 + 1", input_sequence)
+    input_sequence = input_sequence.replace("public static void main(String[] args)\n{\n","\n\n")
+    input_sequence = input_sequence.replace("\n}\n","")
+    input_sequence = input_sequence.replace("public static division (int a, int b) {","\n\n")
+    input_sequence = input_sequence.replace("System.out.println(","alert(")
     f.close()
 
     i = 0
@@ -275,6 +281,328 @@ def prog1():
     f = open('tokens.txt', 'w')
     f.write(output_sequence)
     f.close()
+
+def analyzer():
+    global i
+    global nxtsymb
+    global row_counter
+    i = -1
+    nxtsymb = None # разбираемый символ
+    row_counter = 1 # счётчик строк
+
+    # лексемы
+    tokens = {'W': {}, 'I': {}, 'O': {}, 'R': {}, 'N': {}, 'C': {}}
+
+    # файлы, содержащие все таблицы лексем
+    for token_class in tokens.keys():
+        with open('%s.json' % token_class, 'r') as read_file:
+            data = json.load(read_file)
+            tokens[token_class] = data
+
+    # файл, содержащий последовательность кодов лексем входной программы
+    f = open('tokens.txt', 'r')
+    input_sequence = f.read()
+    f.close()
+
+    regexp = '[' + '|'.join(tokens.keys()) + ']' + '\d+'
+    match = re.findall(regexp, input_sequence)
+
+    # обработка ошибочной ситуации
+    def error():
+        out_sq = 'Ошибка в строке '
+        f = open('error.txt','w')
+        out_sq += str(row_counter)
+        f.write(out_sq)
+        f.close()
+        #print('Ошибка в строке', row_counter)
+        return
+
+
+    # помещение очередного символа в nxtsymb
+    def scan():
+        global i, nxtsymb, row_counter
+        i += 1
+        if i >= len(match):
+            if not(nxtsymb in ['\n', ';', '}']):
+                error()
+        else:
+            for token_class in tokens.keys():
+                if match[i] in tokens[token_class]:
+                    nxtsymb = tokens[token_class][match[i]]
+            if nxtsymb == '\n':
+                row_counter += 1
+                scan()
+            #print(i, row_counter, nxtsymb)
+
+    # программа
+    def program():
+        operators()
+
+    # операторы
+    def operators():
+        global i
+        scan()
+        while name() or \
+            nxtsymb in ['int', 'double', 'string', 'boolean', 'float', '{', 'public static void main(String[] args)',\
+                        'if', 'for', 'while', 'break', 'continue', 'return']:
+            operator()
+            if nxtsymb == ';':
+                scan()
+            if nxtsymb == '}':
+                break
+
+    # оператор
+    def operator():
+        if nxtsymb in ['int','double','float','boolean','string']:
+            description()
+            if nxtsymb != ';': error()
+        elif name():
+            scan()
+            if nxtsymb == ':':
+                scan()
+                operator()
+            elif nxtsymb == '(':
+                scan()
+                if nxtsymb != ')':
+                    expression()
+                    while nxtsymb == ',':
+                        scan()
+                        expression()
+                    if nxtsymb != ')': error()
+                scan()
+            elif nxtsymb == '=':
+                scan()
+                expression()
+                if nxtsymb != ';': error()
+            else: error()
+        elif nxtsymb == '{': compound_operator()
+        elif nxtsymb == 'public static': function()
+        elif nxtsymb == 'if': conditional_operator()
+        elif nxtsymb == 'for': for_loop()
+        elif nxtsymb == 'while': while_loop()
+        elif nxtsymb == 'break':
+            break_operator()
+            scan()
+            if nxtsymb != ';': error()
+        elif nxtsymb == 'continue':
+            continue_operator()
+            scan()
+            if nxtsymb != ';': error()
+        elif nxtsymb == 'return':
+            return_operator()
+            if nxtsymb != ';': error()
+        else: error()
+
+    # имя (идентификатор)
+    def name():
+        return nxtsymb in tokens['I'].values() or \
+            nxtsymb in ['System.out.println', 'alert']
+
+    # описание
+    def description():
+        scan()
+        if not(name()): error()
+        scan()
+        if nxtsymb == ',':
+            while nxtsymb == ',':
+                scan()
+                if not(name()): error()
+                scan()
+        elif nxtsymb == '=':
+            scan()
+            if nxtsymb == 'new':
+                scan()
+                if not(name()): error()
+                if not(nxtsymb == '['): error()
+                scan()
+                if not(integer()): error()
+                scan()
+                if not(nxtsymb == ']'): error()
+                scan()
+            else:
+                expression()
+
+    # список имен
+    def list_of_names():
+        if not(name()): error()
+        scan()
+        while nxtsymb == ',':
+            scan()
+            if not(name()): error()
+            scan()
+
+    # функция
+    def function():
+        if nxtsymb != 'public static void main(String[] args)\n{': error()
+        scan()
+        if not(name()): error()
+        scan()
+        if nxtsymb == '(':
+            scan()
+            if name():
+                list_of_names()
+        if nxtsymb != ')': error()
+        scan()
+        compound_operator()
+
+    # выражение
+    def expression():
+        if nxtsymb == '(':
+            scan()
+            expression()
+            if nxtsymb != ')': error()
+            scan()
+        elif name():
+            scan()
+            if nxtsymb == '(':
+                scan()
+                if nxtsymb != ')':
+                    expression()
+                    while nxtsymb == ',':
+                        scan()
+                        expression()
+                    if nxtsymb != ')': error()
+                scan()
+            elif nxtsymb == '[':
+                scan()
+                expression()
+                while nxtsymb == ',':
+                    scan()
+                    expression()
+                if nxtsymb != ']': error()
+                scan()
+        elif number() or line(): scan()
+        else: error()
+        if arithmetic_operation():
+            scan()
+            expression()
+
+    # число (числовая константа)
+    def number():
+        return nxtsymb in tokens['N'].values()
+
+    # целое число (числовая константа)
+    def integer():
+        return nxtsymb in tokens['N'].values()
+
+    # строка (символьная константа)
+    def line():
+        return nxtsymb in tokens['C'].values()
+
+    # переменная
+    def variable():
+        if not(name()): error()
+        scan()
+        if nxtsymb == '[':
+            scan()
+            expression()
+            if nxtsymb != ']': error()
+            scan()
+
+    # арифметическая операция
+    def arithmetic_operation():
+        return nxtsymb in ['%', '*', '+', '-', '/', '+=','-=', '*=', '/=', '%=']
+
+    # составной оператор
+    def compound_operator():
+        if nxtsymb != '{': error()
+        operators()
+        if nxtsymb != '}': error()
+        scan()
+
+    # оператор присваивания
+    def assignment_operator():
+        scan()
+        variable()
+        if nxtsymb != '=': error()
+        scan()
+        expression()
+
+    # условный оператор
+    def conditional_operator():
+        if nxtsymb != 'if': error()
+        scan()
+        if nxtsymb != '(': error()
+        condition()
+        if nxtsymb != ')': error()
+        scan()
+        operator()
+        if nxtsymb == 'else':
+            scan()
+            operator()
+
+    # условие
+    def condition():
+        if unary_log_operation():
+            scan()
+            if nxtsymb != '(': error()
+            log_expression()
+            if nxtsymb != ')': error()
+            scan()
+        else:
+            log_expression()
+            while binary_log_operation():
+                log_expression()
+
+    # унарная логическая операция
+    def unary_log_operation():
+        return nxtsymb == '!'
+
+    # логическое выражение
+    def log_expression():
+        scan()
+        expression()
+        comparison_operation()
+        scan()
+        expression()
+
+    # операция сравнения
+    def comparison_operation():
+        return nxtsymb in ['!=', '<', '<=', '==', '>', '>=']
+
+    # бинарная логическая операция
+    def binary_log_operation():
+        return nxtsymb == '&&' or nxtsymb == '||'
+
+    # цикл for
+    def for_loop():
+        if nxtsymb != 'for': error()
+        scan()
+        if nxtsymb != '(': error()
+        assignment_operator()
+        if nxtsymb != ';': error()
+        condition()
+        if nxtsymb != ';': error()
+        assignment_operator()
+        if nxtsymb != ')': error()
+        scan()
+        operator()
+
+    # цикл while
+    def while_loop():
+        if nxtsymb != 'while': error()
+        scan()
+        if nxtsymb != '(': error()
+        condition()
+        if nxtsymb != ')': error()
+        scan()
+        operator()
+
+    # оператор break
+    def break_operator():
+        return nxtsymb == 'break'
+
+    # оператор continue
+    def continue_operator():
+        return nxtsymb == 'continue'
+
+    # оператор return
+    def return_operator():
+        if nxtsymb != 'return': error()
+        scan()
+        expression()
+
+    program()
 
 def prog2():
     #LAB1
@@ -1645,8 +1973,50 @@ def clicked3():
         if file != filename:
             os.remove(os.path.join(dir_path, file))
 
+def clicked4():
+    write_txt(codetxt4.get("1.0","end"))
+    codetxt4.tag_remove("highlight", "1.0", "end")
 
-window=Tk()
+    errorlb.config(text=" ")
+
+    prog1()
+    analyzer()
+
+    dir_path = os.getcwd()
+
+    ername = 'error.txt'
+    for file in os.listdir(dir_path):
+        if file == ername:
+            f = open("error.txt",'r')
+            err = f.read()
+            errorlb.config(text= err)
+            f.close()
+
+            with open(ername) as f:
+                for line in f:
+                    match = re.search(r'\d+', line)
+                    if match:
+                        line_num = int(match.group())
+                        highlight_line(codetxt4, line_num)
+            break
+        else:
+            errorlb.config(text= 'Ошибок нет')
+
+    filename = 'Trans.py'
+    for file in os.listdir(dir_path):
+        if file != filename:
+            os.remove(os.path.join(dir_path, file))
+
+def highlight_line(codetxt4, line_num):
+    codetxt4.tag_remove("highlight", "1.0", "end")
+
+    start = f"{line_num}.0"
+    end = f"{line_num + 1}.0"
+
+    codetxt4.tag_add("highlight", start, end)
+    codetxt4.tag_config("highlight", background="red")
+
+window = Tk()
 window.title("Транслятор")
 window.geometry('1100x500')
 
@@ -1658,11 +2028,13 @@ notebook.pack(fill='both', expand=True)
 LR1 = ttk.Frame(notebook)
 LR2 = ttk.Frame(notebook)
 LR3 = ttk.Frame(notebook)
+LR4 = ttk.Frame(notebook)
 
 # Добавляем вкладки в Notebook
 notebook.add(LR1, text='Токенизация')
 notebook.add(LR2, text='Обратная польская нотация')
 notebook.add(LR3, text='Перевод из Java в C++')
+notebook.add(LR4, text='Синтаксический анализатор')
 
 # Токены
 codetxt1=st.ScrolledText(LR1)
@@ -1723,5 +2095,17 @@ cpptext.place(x=600, y=0, width=410, height=390)
 
 btngo = tk.Button(LR3, text="Выполнить \n преобразование", command=clicked3, font=("Arial", 10))
 btngo.place(x=470, y=90, width=110, height=50)
+
+# Анализатор
+codetxt4 = st.ScrolledText(LR4)
+codetxt4.place(x=300, y=0, width=410, height=390)
+
+codetxt4.tag_configure("highlight", background="red")
+
+btngo = tk.Button(LR4, text="Проверить \n правильность кода", command=clicked4, font=("Arial", 10))
+btngo.place(x=150, y=86, width=130, height=50)
+
+errorlb = Label(LR4,text="Ошибок нет",font=("Arial", 12))
+errorlb.place(x=400,y=400)
 
 window.mainloop()
